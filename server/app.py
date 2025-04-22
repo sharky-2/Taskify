ce48767e16a48ac7e4b40ebad8ae71bde1a86518
sharky-2 <blazmedvode@gmail.com>
2025-04-22 21:20:55 +0200

    welcome page

--- server/app.py
+++ server/app.py
@@ -8,16 +8,16 @@ from dotenv import load_dotenv
 app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def index():
-    return render_template("index.html")
+    return render_template("welcome.html")
 
 def run_flask():
     app.run(debug=False, use_reloader=False)
 
 if __name__ == "__main__": 
 
-    app.run(debug=True) 
+    # app.run(debug=True) 
 
-    # flask_thread = threading.Thread(target=run_flask)
-    # flask_thread.start()
-    # webview.create_window("App", "http://127.0.0.1:5000", width=500, height=900)
-    # webview.start()
+    flask_thread = threading.Thread(target=run_flask)
+    flask_thread.start()
+    webview.create_window("Taskify", "http://127.0.0.1:5000", width=600, height=900)
+    webview.start()
