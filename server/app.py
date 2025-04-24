d1d85ee745017ac8ba4c3f0498c53df745e315d7
sharky-2 <blazmedvode@gmail.com>
2025-04-24 14:12:31 +0200

    small fix

--- server/app.py
+++ server/app.py
@@ -3,29 +3,21 @@ import os
 import threading
 
 from flask import Flask, render_template, jsonify, request, redirect, url_for, session
-from dotenv import load_dotenv
 
 app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def welcome():
-    return render_template("tutorial.html")
-
-@app.route("/registration")
-def registration():
-    return render_template("registration.html")
-
-@app.route("/index")
-def index():
     return render_template("index.html")
+
 
 def run_flask():
     app.run(debug=False, use_reloader=False)
 
 if __name__ == "__main__": 
 
-    # app.run(debug=True) 
+    app.run(debug=True) 
 
-    flask_thread = threading.Thread(target=run_flask)
-    flask_thread.start()
-    webview.create_window("Taskify", "http://127.0.0.1:5000", width=550, height=870, frameless=True)
-    webview.start()
+    # flask_thread = threading.Thread(target=run_flask)
+    # flask_thread.start()
+    # webview.create_window("Taskify", "http://127.0.0.1:5000", width=550, height=870, frameless=True)
+    # webview.start()
